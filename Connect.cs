fa3c74fd4b2b3fb9a972a197665c7a897ea37c54
Andile Dimba <mlamuliforex@gmail.com>
2022-04-28 09:07:51 +0200

    added count and write to file

--- Connect.cs
+++ Connect.cs
@@ -1,23 +1,24 @@
-using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore;
 
-namespace ConnectToSQLServer
-{
-    public class InternContext : DbContext
-    {
-        public InternContext(DbContextOptions<InternContext> options) : base(options)
-        {
+// namespace ConnectToSQLServer
+// {
+//     public class InternContext : DbContext
+//     {
+//         public InternContext(DbContextOptions<InternContext> options) : base(options)
+//         {
 
-        }
-        public DbSet<Intern> Interns { get; set; }
-    }
+//         }
+//         // public DbSet<Intern> Interns { get; set; }
+//         public DbSet<Intern> Interns => Set<Intern>();
+//     }
 
-    public class Intern
-    {
-        public int InternID { get; set; }
-        public string FirstName { get; set; }
+//     public class Intern
+//     {
+//         public int InternID { get; set; }
+//         public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+//         public string LastName { get; set; }
 
-    }
+//     }
 
-}
+// }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using ConnectToSQLServer;
 using System.Data.SqlClient;
 
 
@@ -35,6 +34,47 @@ app.MapDelete("/Delete/{id}", async (InternContext db, int id) =>
     return Results.NoContent();
 });
 
+app.MapGet("/Calc", (InternContext db) =>
+{
+ int count = (from intern in db.Interns where intern.YearOfInternship < 2010 select intern).Count();
+ return count;
+});
+
+app.MapGet("/WriteToFile", async (InternContext db) =>
+{
+ List<Intern> interns = await db.Interns.ToListAsync();
+ using StreamWriter file = new("Interns.txt");
+
+ try{
+     foreach(Intern person in interns){
+         await file.WriteLineAsync(person.FirstName + " " + person.LastName);
+     }
+ }
+     catch( Exception err){
+         Console.WriteLine("Error Message is " + err.Message);
+     }
+ });
 
 
 app.Run();
+
+public class InternContext : DbContext
+    {
+        public InternContext(DbContextOptions<InternContext> options) : base(options)
+        {
+
+        }
+        // public DbSet<Intern> Interns { get; set; }
+        public DbSet<Intern> Interns => Set<Intern>();
+    }
+
+    public class Intern
+    {
+        public int InternID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int YearOfInternship { get; set; }
+
+    }
